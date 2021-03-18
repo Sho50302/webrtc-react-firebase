4dbe492f1e2cb03c2268ed91034ed5d9f4457a00
Sho50302 <61681052+Sho50302@users.noreply.github.com>
2021-03-18 15:15:58 +0900

    create signin page for remotePeerName

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import InputFormLocal from './InputFormLocal';
 import InputFormRemote from './InputFormRemote';
@@ -11,17 +11,26 @@ const getMedia = async () => {
   } catch (err) {
     console.error(err);
   }
-};
+}
 
 getMedia();
 
 const App = () => {
+  const [localPeerName, setLocalPeerName] = useState('');
+  const [remotePeerName, setRemotePeerName] = useState('');
+
   return (
     <React.Fragment>
-      <InputFormLocal />
-      <InputFormRemote />
+      <InputFormLocal
+        localPeerName={localPeerName}
+        setLocalPeerName={setLocalPeerName}
+      />
+      <InputFormRemote
+        remotePeerName={remotePeerName}
+        setRemotePeerName={setRemotePeerName}
+      />
     </React.Fragment>
-  )
-};
+  );
+}
 
 export default App;
