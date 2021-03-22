c90075aefbef373bf6f9b829b9d75efd8451dd5e
Sho50302 <61681052+Sho50302@users.noreply.github.com>
2021-03-22 22:00:56 +0900

    create video area

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,7 @@ import React, { useState } from 'react';
 
 import InputFormLocal from './InputFormLocal';
 import InputFormRemote from './InputFormRemote';
-
-const getMedia = async () => {
-  const constraints = { audio: true, video: true };
-
-  try {
-    return await navigator.mediaDevices.getUserMedia(constraints);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-getMedia();
+import VideoArea from './VideoArea';
 
 const App = () => {
   const [localPeerName, setLocalPeerName] = useState('');
@@ -30,6 +19,10 @@ const App = () => {
         localPeerName={localPeerName}
         remotePeerName={remotePeerName}
         setRemotePeerName={setRemotePeerName}
+      />
+      <VideoArea
+        localPeerName={localPeerName}
+        remotePeerName={remotePeerName}
       />
     </React.Fragment>
   );
