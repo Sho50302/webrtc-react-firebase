28e6456a11f6464e14763eb248c1702fbb572a22
Sho50302 <61681052+Sho50302@users.noreply.github.com>
2021-04-25 17:48:02 +0900

    create setMediaStream

--- src/utils/RtcClient.js
+++ src/utils/RtcClient.js
@@ -1,5 +1,6 @@
 import FirebaseSignallingClient from './FirebaseSignallingClient';
 
+// RtcClientクラス
 export default class RtcClient {
   constructor(setRtcClient) {
     const config = {
@@ -13,10 +14,12 @@ export default class RtcClient {
     this.mediaStream = null;
   }
 
+  // RtcClientの値を更新
   setRtcClient() {
     this._setRtcClient(this);
   }
 
+  // mediaStreamを非同期で取得
   async getUserMedia() {
     try {
       const constraints = { audio: true, video: true };
@@ -26,6 +29,40 @@ export default class RtcClient {
     }
   }
 
+  // mediaStreamの初期設定
+  async setMediaStream() {
+    await this.getUserMedia();
+    this.addTracks();
+    this.setRtcClient();
+  }
+
+  // 各Trackの取得
+  addTracks() {
+    this.addAudioTrack();
+    this.addVideoTrack();
+  }
+
+  // mediaStreamに音声Trackの追加
+  addAudioTrack() {
+    this.rtcPeerConnection.addTrack(this.audioTrack, this.mediaStream);
+  }
+
+  // mediaStreamに映像Trackの追加
+  addVideoTrack() {
+    this.rtcPeerConnection.addTrack(this.videoTrack, this.mediaStream);
+  }
+
+  // 音声の取得
+  get audioTrack() {
+    return this.mediaStream.getAudioTracks()[0];
+  }
+
+  // 映像の取得
+  get videoTrack() {
+    return this.mediaStream.getVideoTracks()[0];
+  }
+
+  // Listen時の初期設定
   startListning(localPeerName) {
     this.localPeerName = localPeerName;
     this.setRtcClient();
