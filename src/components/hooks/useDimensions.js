5c0c2a63b11eaabed4a5845920ab5565b0d9a70f
Sho50302 <61681052+Sho50302@users.noreply.github.com>
2021-05-01 11:56:32 +0900

    create mute button

--- src/components/hooks/useDimensions.js
+++ src/components/hooks/useDimensions.js
@@ -5,6 +5,7 @@ const defaultDimensions = { width: 0, height: 0 }
 const useDimensions = (targetRef) => {
   let [dimensions, setDimensions] = useState(defaultDimensions);
   const node = targetRef.current;
+  console.log({ node })
   const updateDimensions = (node) => {
     return node === null
       ? defaultDimensions
